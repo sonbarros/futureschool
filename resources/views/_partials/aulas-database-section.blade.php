4227670630d1c3f2418c675448e559a9252a7810
Anderson de S Barros <anderson21br@gmail.com>
2022-07-29 21:54:15 -0300

    aplicando o link para download na view

--- resources/views/_partials/aulas-database-section.blade.php
+++ resources/views/_partials/aulas-database-section.blade.php
@@ -26,19 +26,21 @@
         <div class="flex sm:justify-end w-full">
           <ul class="space-y-2 mt-2 sm:mt-0 w-full sm:w-52">
             @foreach($aula->files as $file)
-              <li>
-                <button class="inline-flex h-12 items-center justify-between border-0 w-full focus:outline-none transition ease-in-out duration-150 text-white bg-white hover:bg-gray-50 border-white hover:border-gray-50 text-xs px-2.5  rounded">
-                  <span class="font-light text-brand">{{$file->description}}</span>
-                  <div class="flex items-center sm:justify-end justify-between w-16" style="">
-                    <svg  xmlns="http://www.w3.org/2000/svg" class="inline-block sm:hidden h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                      <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
-                    </svg>
-                    <img 
-                    @if($file->icon->url == '_img/media-player.webp') class="w-10" @else  class="w-6 mr-2" @endif
-                    src="{{asset( $file->icon->url )}}" alt="">
-                  </div>
-                </button>
-              </li>
+              @isset($file->url)<!-- apresenta link para download apenas quando o url estive endereço da imagem -->
+                <li>
+                  <a href="{{asset( $file->url )}}" download class="inline-flex h-12 items-center justify-between border-0 w-full focus:outline-none transition ease-in-out duration-150 text-white bg-white hover:bg-gray-50 border-white hover:border-gray-50 text-xs px-2.5  rounded">
+                    <span class="font-light text-brand">{{$file->description}}</span>
+                    <div class="flex items-center sm:justify-end justify-between w-16" style="">
+                      <svg  xmlns="http://www.w3.org/2000/svg" class="inline-block sm:hidden h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+                      </svg>
+                      <img 
+                      @if($file->icon->url == 'storage/_img/ico-mediaplayer.webp') class="w-10" @else  class="w-6 mr-2" @endif
+                      src="{{asset( $file->icon->url )}}" alt="">
+                    </div>
+                  </a>
+                </li>
+              @endisset
             @endforeach
           </ul>
         </div>
