5c61fa3b0f70c57bc030bafe817b81234845ae8b
Anderson de S Barros <anderson21br@gmail.com>
2022-08-08 10:48:57 -0300

    Atualizando link pagina sobre

--- resources/views/menutop.blade.php
+++ resources/views/menutop.blade.php
@@ -15,7 +15,7 @@
         <a href="{{route('curso.show', 'cobol')}}" class="ml-4 px-3 py-2 rounded-md text-sm leading-5 font-medium text-gray-50 bg-yellow-500 hover:bg-blue-600 hover:text-white transition duration-150 ease-in-out cursor-pointer focus:outline-none focus:text-white focus:bg-gray-700 "> COBOL </a>
         <a href="{{route('curso.show', 'cics')}}" class="ml-4 px-3 py-2 rounded-md text-sm leading-5 font-medium text-gray-50 bg-yellow-500 hover:bg-blue-600 hover:text-white transition duration-150 ease-in-out cursor-pointer focus:outline-none focus:text-white focus:bg-gray-700 "> CICS </a>
         <a href="{{route('curso.show', 'db2')}}" class="ml-4 px-3 py-2 rounded-md text-sm leading-5 font-medium text-gray-50 bg-yellow-500 hover:bg-blue-600 hover:text-white transition duration-150 ease-in-out cursor-pointer focus:outline-none focus:text-white focus:bg-gray-700 "> DB2 </a>
-        <a href="http://osasconamao.com.br/CURSOS/_imagens/vitrine2.ppsx" download class="whitespace-nowrap ml-4 px-3 py-2 rounded-md text-sm leading-5 font-medium text-gray-50 bg-yellow-500 hover:bg-blue-600 hover:text-white transition duration-150 ease-in-out cursor-pointer focus:outline-none focus:text-white focus:bg-gray-700 "> QUEM SOMOS </a>
+        <a href="https://s3.amazonaws.com/futureschool.com/storage/_arquivos/vitrine2.ppsx" download class="whitespace-nowrap ml-4 px-3 py-2 rounded-md text-sm leading-5 font-medium text-gray-50 bg-yellow-500 hover:bg-blue-600 hover:text-white transition duration-150 ease-in-out cursor-pointer focus:outline-none focus:text-white focus:bg-gray-700 "> QUEM SOMOS </a>
       </div>
     </div>
     
