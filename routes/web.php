c4cceff494e1bb6f470358b68d629bfcdc3ed95b
Anderson Sousa <anderson21br@gmail.com>
2022-08-26 18:22:54 -0300

    Incluido pacote auth

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ use App\Http\Controllers\FileController;
 Route::get('/', [CursoController::class, 'index'])->name('site.index');
 Route::get('/curso/{name}', [CursoController::class, 'show'])->name('curso.show');
 Route::get('/file/{id}', [FileController::class, 'show'])->name('file.show');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,4 @@ mix.js('resources/js/app.js', 'public/js')
 .postCss('resources/css/app.css', 'public/css', [
     require("tailwindcss"),
 ]);
-    //.sass('resources/sass/app.scss', 'public/css');
+//.sass('resources/sass/app.scss', 'public/css');
